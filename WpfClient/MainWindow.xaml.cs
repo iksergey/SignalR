67a8d6ec0efab317188865e86d76eff4886e4860
Sergei Kamianetskii <i@ksergey.ru>
2023-04-28 06:11:39 +0300

    Добавлен WPF клиент

---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClient
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    WpfApp wpf;
+    public MainWindow()
+    {
+      InitializeComponent();
+      wpf = new WpfApp(this);
+      wpf.Start();
+      list.ItemsSource = wpf.Storage.Read();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      wpf.Send(txt.Text);
+    }
+  }
+}
---
+++ WpfClient/WpfApp.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatLibrary;
+using System;
+using System.Windows;
+
+namespace WpfClient
+{
+  public class WpfApp
+  {
+    string clientId;
+    UserModel person;
+    ChatCore<UserModel> chat;
+    IStorage<UserModel> storage;
+    public IStorage<UserModel> Storage => storage;
+    Window window;
+    public WpfApp(Window w)
+    {
+      clientId = Guid.NewGuid().ToString()[..5];
+      window = w;
+    }
+    private void UserInit()
+    {
+      person = new()
+      {
+        Username = $"user {clientId}"
+      };
+    }
+
+    private void UIConfig()
+    {
+      window.Title = $"[{clientId}] chat";
+    }
+    private void AppConfig()
+    {
+      storage = new Storage<UserModel>();
+      string url = "https://localhost:5001/notify"; ;
+      chat = new(url, storage, new DebugUI());
+      chat.StartReceive();
+    }
+    public void Start()
+    {
+      UserInit();
+      UIConfig();
+      AppConfig();
+    }
+
+    public void Send(string tm)
+    {
+      person.TextMessage = tm;
+      chat.ClientSendMessage(person);
+    }
+  }
+}
